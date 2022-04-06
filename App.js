4f8c21c0446d9905e14f0e31980a1d9b9f13c823
AlisonChs <alison.carvalho@etec.sp.gov.br>
2022-04-06 14:59:17 -0300

    Criação do componente 'Jogos'

--- App.js
+++ App.js
@@ -1,17 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
 import Cabecalho from './src/components/Cabecalho';
 import Botao from './src/components/Botao';
+import Jogo from './src/components/Jogo';
+import Dados from './src/Dados';
 
 export default function App() {
   return (
     <view>
       <Cabecalho />
+
       <Botao 
-      Logo= "md-game-controller"
-      texto="Jogos"
-      cor="black"
+        Logo= "md-game-controller"
+        texto="Jogos"
+        cor="black"
       />
+
+      <FlatList
+        horizontal={true}
+        data = {Dados}
+        keyExtractor = {(item) => item.id}
+        renderItem = { ({ item }) => (
+
+        <Jogo
+          titulo ={item.nome}
+          imagem ={item.imagem}
+          valor ={item.valor}
+        />
+
+        )}
+
+        />
+
     </view>
   );
 }
---
+++ src/Dados/index.js
@@ -0,0 +1,45 @@
+const JOGOS = [
+    {
+        id : "01",
+        nome: "Subway Surfers",
+        valor: 13.32,
+        imagem: 'subway.jpg',
+    },
+
+    {
+        id : "02",
+        nome: "Yugioh Master Duel",
+        valor: "gratuito", 
+        imagem: "yugi.jpg",
+    },
+
+    {
+        id : '03',
+        nome: 'Pokemon go',
+        valor: 'gratuito',
+        imagem: 'pokemon.jpg',
+    },
+
+    {
+        id : '04',
+        nome: 'Hora da aventura',
+        valor: 'R$10,00',
+        imagem: 'aventura.jpg', 
+    },
+
+    {
+        id:'05',
+        nome:'Dragon Ball Fight',
+        valor:'R$18,00',
+        imagem:'dbz.jpg',
+    },
+    
+    {
+        id: '06',
+        nome: 'Super Paper Mario',
+        valor: 'R$ 23,00',
+        imagem: 'superPaperMario.png',
+    }
+
+]
+export default JOGOS;
