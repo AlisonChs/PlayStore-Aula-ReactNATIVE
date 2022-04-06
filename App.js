59adb17eb24d87dc864cab454045b5345df6dfb8
Alison Christian <alison.carvalho@etec.sp.gov.br>
2022-04-06 02:46:34 -0300

    Criação do componente 'botão'

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Cabecalho from './src/components/cabecalho';
+import Cabecalho from './src/components/Cabecalho';
+import Botao from './src/components/Botao';
 
 export default function App() {
   return (
     <view>
       <Cabecalho />
+      <Botao 
+      Logo= "md-game-controller"
+      texto="Jogos"
+      cor="black"
+      />
     </view>
   );
 }
@@ -18,3 +24,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
